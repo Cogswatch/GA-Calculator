f0c42ffa04916b638c1cc75b8700aa457c2126c9
C. Greyston Brady <c.greyston.brady@gmail.com>
2021-06-06 22:12:55 -0700

    Added test cases from CI-calculator in unittest format

--- test_calculator.py
+++ test_calculator.py
@@ -2,4 +2,14 @@ import calculator
 import unittest
 
 class TestCase(unittest.TestCase):
-  
+    def test_add(self):
+        self.assertEqual(calculator.add(2, 2), 4)
+    def test_sub(self):
+        self.assertEqual(calculator.sub(7, 4), 3)
+    def test_mul(self):
+        self.assertEqual(calculator.mul(8, 3), 24)
+    def test_div(self):
+        self.assertEqual(calculator.div(24, 8), 3)
+
+if __name__ == '__main__':
+    unittest.main()
